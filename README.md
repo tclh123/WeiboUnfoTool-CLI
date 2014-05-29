6482a71d823c78a6579159d07eea192f698299b9
tclh123 <tclh123@gmail.com>
2014-05-29 17:41:39 +0800

    add client

---
+++ README.md
@@ -0,0 +1,19 @@
+
+
+## Usage
+
+### Config
+
+```
+cp weibounfo/local_config.py.example weibounfo/local_config.py
+```
+
+### Get access_token
+
+```
+./weibounfo/get_access_token.py
+# then input the code (the query string of the redirect url)
+# access_token.bak will be saved
+```
+
+### Next..
---
+++ weibounfo/client.py
@@ -0,0 +1,54 @@
+# coding: utf-8
+
+import os
+from weibounfo.lib.snspy import APIClient
+from weibounfo.lib.snspy import SinaWeiboMixin
+
+from weibounfo.config import APP_KEY, APP_SECRET, CALLBACK_URL
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def get_access_token():
+    try:
+        with open(os.path.join(here, 'access_token.bak'), 'r') as f:
+            text = f.read()
+            res = text.splitlines()
+        return res
+    except Exception as e:
+        print 'Error with get_access_token()'
+        raise e
+
+access_token, expires = get_access_token()
+
+
+def client_with_access_token(a):
+    c = APIClient(SinaWeiboMixin,
+                  app_key=APP_KEY, app_secret=APP_SECRET,
+                  redirect_uri=CALLBACK_URL,
+                  access_token=a, expires=expires)
+    return c
+
+client = client_with_access_token(access_token)
+
+
+def test_friends():
+    idx = [0]
+
+    def f(cursor):
+        r = client.friendships.friends.get(uid=1659177872, count=200, cursor=cursor)
+        for u in r.users:
+            idx[0] += 1
+            print '%s: %s' % (idx[0], u.screen_name)
+        return r.next_cursor
+
+    c = f(0)
+    while c:
+        print c
+        c = f(c)
+
+    print idx[0]
+
+
+if __name__ == '__main__':
+    test_friends()
